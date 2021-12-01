9719875495b4a6f9327fe2798f3c6b3e338b4a15
Bastian Hofmann <bashofmann@gmail.com>
2021-12-01 10:21:26 +0100

    Demo

---
+++ 02-install_certmanager.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+export KUBECONFIG=$(pwd)/kubeconfig
+
+helm repo add jetstack https://charts.jetstack.io
+
+helm upgrade --install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --set installCRDs=true \
+  --version v1.5.2 --create-namespace
+
+kubectl rollout status deployment -n cert-manager cert-manager
+kubectl rollout status deployment -n cert-manager cert-manager-webhook
+
+kubectl apply -f cert-manager/cluster-issuer.yaml
